86650eb2cd11d4ea3257ea8cbf7e3835213bf979
electroP0L <paul.roger-dauvergne@etu.sorbonne-universite.fr>
2024-11-07 15:41:39 +0100

    First dotfiles and custom files

---
+++ install-scripts/create-symlinks.sh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+# Inspired from https://github.com/mattjmorrison/dotfiles/blob/master/install-scripts/Linux/create-symlinks.sh
+#===============================================================================
+#
+#             NOTES: For this to work you must have cloned the github
+#                    repo to your home folder as ~/dotfiles/
+#
+#===============================================================================
+
+#==============
+# Variables
+#==============
+dotfiles_dir=~/dotfiles
+
+#==============
+# Rename existing dot files and folders (append .old)
+#==============
+for name in ~/.bashrc ~/.zshrc; do
+    if [ -e "$name" ]; then
+        mv "$name" "$name.old" 2>/dev/null
+    fi
+done
+
+#==============
+# Create symlinks in the home folder
+# Allow overriding with files of matching names in the custom-configs dir
+#==============
+ln -sf $dotfiles_dir/bash/.bashrc ~/.bashrc
+ln -sf $dotfiles_dir/zsh/.zshrc ~/.zshrc
+
+#==============
+# Set zsh as the default shell
+#==============
+sudo chsh -s $(which zsh)
